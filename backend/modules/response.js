8cd33d6cbc06a3d3439d31cedde2987a28ee720b
MarcusLages <marcusvlages@gmail.com>
2025-10-08 12:58:19 -0700

    Updated response module in backend to send response, instead of creating response, and optimized the templates

--- backend/modules/response.js
+++ backend/modules/response.js
@@ -1,35 +1,49 @@
 const { MSGS, DEFAULT_BAD_REQ_ERR_KEY, DEFAULT_NOT_FOUND_ERR_KEY } = require("../lang/messages/en/user.js")
 
 exports.Response = class Response {
-    static successRes(data, code) {
-        return {
+    
+    static RES_CONTENT_TYPE = "application/json";
+    
+    static sendResponse(res, code, data) {
+        res.writeHead(code, { "Content-Type": this.RES_CONTENT_TYPE });
+        res.end(data);
+    } 
+
+    static successRes(res, data, code) {
+        const res_data = {
             status: "success",
             code: code || 200,
             data: data
-        }
+        };
+        this.sendResponse(res, res_data, code);
     }
 
-    static errorRes(err_msg, err_code) {
-        return {
+    static createdRes(res, data) {
+        this.sendResponse(res, data, 201);
+    }
+
+    static errorRes(res, err_msg, err_code) {
+        const res_data = {
             status: "error",
             code: err_code || 400,
             data: err_msg
-        }
+        };
+        this.sendResponse(res, res_data, res_data.code);
     }
 
-    static badReqError(err_msg) {
-        return {
-            status: "error",
-            code: 402,
-            data: err_msg || MSGS[DEFAULT_BAD_REQ_ERR_KEY]
-        }
+    static badReqError(res, err_msg) {
+        this.errorRes(
+            res,
+            err_msg || err_msg || MSGS[DEFAULT_BAD_REQ_ERR_KEY],
+            402
+        );
     }
-
-    static notFoundError(err_msg) {
-        return {
-            status: "error",
-            code: 404,
-            data: err_msg || MSGS[DEFAULT_NOT_FOUND_ERR_KEY]
-        }
+    
+    static notFoundError(res, err_msg) {
+        this.errorRes(
+            res,
+            err_msg || err_msg || MSGS[DEFAULT_NOT_FOUND_ERR_KEY],
+            404
+        );
     }
 }
