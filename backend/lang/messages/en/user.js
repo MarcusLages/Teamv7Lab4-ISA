6f5151a55707bd12e6e92530ca4cf60f6f631b34
MarcusLages <marcusvlages@gmail.com>
2025-10-08 14:07:52 -0700

    Added extra messages to backend/user.js messages

--- backend/lang/messages/en/user.js
+++ backend/lang/messages/en/user.js
@@ -1,9 +1,15 @@
 exports.DEFAULT_BAD_REQ_ERR_KEY = "DEFAULT_BAD_REQ_ERR";
 exports.DEFAULT_NOT_FOUND_ERR_KEY = "DEFAULT_NOT_FOUND_ERR";
 exports.WORD_NOT_FOUND_ERR_KEY = "WORD_NOT_FOUND_ERR";
+exports.WORD_IN_DICT_WARN_KEY = "WORD_IN_DICT_WARN";
+exports.NO_WORD_PARAM_ERR_KEY = "NO_WORD_PARAM_ERR";
+exports.NO_WORD_DEF_PARAM_ERR_KEY = "NO_WORD_DEF_PARAM_ERR";
 
 exports.MSGS = {
     DEFAULT_BAD_REQ_ERR: "Bad request.",
+    NO_WORD_PARAM_ERR: "Bad request. Need `word` parameter in request.",
+    NO_WORD_DEF_PARAM_ERR: "Bad request. Need `word` and `definition` parameters in request.",
     DEFAULT_NOT_FOUND_ERR: "Resource or page not found.",
-    WORD_NOT_FOUND_ERR: "Word not found in dictionary."
+    WORD_NOT_FOUND_ERR: "Word not found in dictionary.",
+    WORD_IN_DICT_WARN: "Word already exists in the dictionary."
 }
